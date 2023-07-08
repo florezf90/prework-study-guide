65d66b5ed1dce5661439c859da1356e7b5c4acb8
luis <florezf90@gmail.com>
2023-07-08 18:35:42 -0500

    fixing my own shit

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -20,7 +20,7 @@ console.log("let's study HTML");
  }
 */
 
- var topics = ["HTML", "CSS", "GIT", "JavaScript"];
+var topics = ["HTML", "CSS", "GIT", "JavaScript"];
 for(var x = 0;  x <topics.length; x++) {
 
     console.log(topics[x]);
