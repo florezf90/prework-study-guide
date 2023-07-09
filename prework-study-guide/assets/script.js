2c1455b22d62d75a79dec54f48e459cd718efe65
luis <florezf90@gmail.com>
2023-07-09 08:21:15 -0500

    we have now added the first functions to the JS file

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,27 +1,45 @@
-/*
-var topic = "HTML";
- if (topic === 'HTML') {
+
+var topics = ['HTML, CSS, GIT, JavaScript']
+
+var randomtopics = topics[Math.floor(Math.random() * topics.length)];
+
+function listoftopics() {
+   
+
+   for(var x = 0;  x <topics.length; x++) {
+   
+       console.log(topics[x]);
+   }
+   }
+   
+function selectopic() {
+   
+ if (topics [0] === 'HTML') {
 console.log("let's study HTML");
 
- } else if (topic === 'CSS') {
+ } else if (topics[0] === 'CSS') {
 
     console.log("let's study CSS!!");
 
- } else if (topic ==  'Git') {
+ } else if (topics [0] ==  'Git') {
 
     console.log("let's study git!!");
 
- } else if (topic === 'JavaScript') {
+ } else if (topics [0] === 'JavaScript') {
 
     console.log("let's study JavaScript!!");
  } else {
 
     console.log("Vamos a fumar marihuana, HPTA!!!!");
  }
-*/
-
-var topics = ["HTML", "CSS", "GIT", "JavaScript"];
-for(var x = 0;  x <topics.length; x++) {
-
-    console.log(topics[x]);
 }
+
+console.log("Here are the topics we have learned through the prework so far:");
+
+listoftopics();
+
+console.log("Which topic are we going to study first??? >:v");
+
+selectopic();
+
+
